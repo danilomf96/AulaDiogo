2a5957dccd9dbb498d9cec6b46cdef7463831f7a
danilomferreira96@gmail.com <autologon@CSED.LAB>
2024-03-14 11:00:57 -0300

    Primeiro commit -- teste

---
+++ Exe06A/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+//criar um vetor
+int[] vetor = new int[100];
+Random numAleatorio = new Random();
+
+// Preencher vetor com valores aleatorios
+for (int i = 0; i < vetor.Length; i++)
+{
+    vetor[i] = numAleatorio.Next(100);
+}
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
+
+bool troca = false;
+do
+{
+    troca = false;
+    for (int i = 0; i < vetor.Length - 1; i++)
+    {
+        if (vetor[i] > vetor[i + 1])
+        {
+            int aux = vetor[i];
+            vetor[i] = vetor[i + 1];
+            vetor[i + 1] = aux;
+            troca = true;
+        }
+    }
+} while (troca == true);
+
+Console.WriteLine("\n");
+
+//Bubble sort
+//Imprimir o vetor
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
---
+++ Exe06B/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.Clear();
+//criar um vetor
+int[] vetor = new int[100];
+Random numAleatorio = new Random();
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    vetor[i] = numAleatorio.Next(100);
+}
+
+Console.WriteLine("Vetor original:");
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
+
+Array.Sort(vetor);
+
+Console.WriteLine("\nVetor ordenado em ordem ascendente:");
+foreach (int i in vetor)
+{
+    Console.Write(i + " ");
+}
+
+Array.Reverse(vetor);
+
+Console.WriteLine("\nVetor ordenado em ordem decrescente:");
+foreach (int i in vetor)
+{
+    Console.Write(i + " ");
+}
